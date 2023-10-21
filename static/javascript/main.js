6d94e5552b2e02d7b8c766ab9b2fdda8f8db89e3
bobpac <141352390+bobpac@users.noreply.github.com>
2023-10-21 16:09:41 -0400

    Add some CSS effects

---
+++ static/javascript/main.js
@@ -0,0 +1,18 @@
+
+function playSpring() {
+  const audio = document.getElementById("spring");
+  console.log(audio);
+  audio.play();
+}
+
+function playSwoosh1() {
+  const audio = document.getElementById("swoosh1");
+  console.log(audio);
+  audio.play();
+}
+
+function playSwoosh2() {
+  const audio = document.getElementById("swoosh2");
+  console.log(audio);
+  audio.play();
+}
